e31e551ebf7a1bfe56f672d3824e79f82efeeeff
andresgutgon <andresgutgon@gmail.com>
2025-02-16 13:58:49 +0100

    Fix mixing dependecies and custom server port

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Coffee Pitch
 Help people connect for potential collaborations in cafes and coworking spaces.
 
+
+## Install deps in development
+Elixir is run inside a docker container. To install the dependencies run the following command:
+```bash
+docker compose run web mix deps.get
+```
+
 ## Tests on the docker container from outside
 
 Run tests in watch mode by running `./bin/test`
--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,7 @@ config :app, AppWeb.Endpoint,
   # Bind to 0.0.0.0 to expose the server to the docker host machine.
   # This makes make the service accessible from any network interface.
   # Change to `ip: {127, 0, 0, 1}` to allow access only from the server machine.
-  http: [ip: {0, 0, 0, 0}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4004],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
