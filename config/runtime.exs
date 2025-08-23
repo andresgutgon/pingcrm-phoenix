20ad8b6d251b86af8fbd5a10ff731bba97abcc74
andresgutgon <andresgutgon@gmail.com>
2025-08-23 17:05:55 +0200

    feat: wip

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,11 +2,13 @@ import Config
 
 env = config_env()
 dev_mode = env == :dev
+prod_mode = env == :prod
 test_dev_mode = env == :test and System.get_env("CI") != "true"
 
 config :pingcrm,
   env: env,
   dev_mode: dev_mode,
+  prod_mode: prod_mode,
   test_dev_mode: test_dev_mode
 
 # config/runtime.exs is executed for all environments, including
@@ -62,22 +64,32 @@ if env == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
+  config :pingcrm, PingcrmWeb.Endpoint,
+    url: [
+      host: System.get_env("PHX_HOST") || "localhost",
+      port: String.to_integer(System.get_env("PORT") || "4004"),
+      scheme: System.get_env("PHX_SCHEME", "http")
+    ]
 
+  endpoint_url = Application.get_env(:pingcrm, PingcrmWeb.Endpoint)[:url]
   config :pingcrm, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :pingcrm, PingcrmWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: endpoint_url[:host], port: endpoint_url[:port], scheme: endpoint_url[:scheme]],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      port: endpoint_url[:port],
     ],
     secret_key_base: secret_key_base
+
+  # Uploads configuration
+  for {app, configuration} <- Pingcrm.Config.Uploads.configure() do
+    config app, configuration
+  end
 
   # ## SSL Support
   #
---
+++ config/uploads.exs
@@ -0,0 +1,49 @@
+defmodule Pingcrm.Config.Uploads do
+  @moduledoc """
+  Configuration for file uploads in Pingcrm.
+
+  In production, files are stored in an S3 bucket compatible storage.
+  In development and test environments, files are stored locally.
+  """
+  def configure do
+    env = Application.get_env(:pingcrm, :env)
+    prod_mode = Application.get_env(:pingcrm, :prod_mode)
+    dev_mode = Application.get_env(:pingcrm, :dev_mode)
+    test_dev_mode = Application.get_env(:pingcrm, :test_dev_mode)
+
+    endpoint_url = Application.get_env(:pingcrm, PingcrmWeb.Endpoint)[:url]
+
+    assets_host =
+      System.get_env("ASSETS_HOST") ||
+        "#{endpoint_url[:scheme]}://#{endpoint_url[:host]}:#{endpoint_url[:port]}"
+
+    cond do
+      prod_mode ->
+        [
+          waffle: [
+            storage: Waffle.Storage.S3,
+            bucket: System.fetch_env!("S3_BUCKET"),
+            asset_host: assets_host
+          ],
+          ex_aws: [
+            access_key_id: System.fetch_env!("S3_ACCESS_KEY_ID"),
+            secret_access_key: System.fetch_env!("S3_SECRET_ACCESS_KEY"),
+            region: System.get_env("S3_REGION", "us-east-1")
+          ]
+        ]
+
+      dev_mode or test_dev_mode or true ->
+        [
+          waffle: [
+            storage: Waffle.Storage.Local,
+            asset_host:
+              if test_dev_mode and System.get_env("CI") == "true" do
+                "http://example.test"
+              else
+                assets_host
+              end
+          ]
+        ]
+    end
+  end
+end
